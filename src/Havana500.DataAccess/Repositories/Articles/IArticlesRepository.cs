8fb073ab0aa06596821519c3922bf95cb976c988
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-08-22 13:41:20 -0400

    Implement the repository and unit test for the Article entity

---
+++ src/Havana500.DataAccess/Repositories/Articles/IArticlesRepository.cs
@@ -0,0 +1,31 @@
+﻿using Havana500.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havana500.DataAccess.Repositories.Articles
+{
+    /// <summary>
+    ///     Declares the functionalities for the 
+    ///     operations on the <see cref="Havana500.Domain.Article"/> entity.
+    /// </summary>
+    public interface IArticlesRepository : IBaseRepository<Article, int>
+    {
+        /// <summary>
+        ///     Increment the amount of views in the Article
+        ///     with Id equal to <paramref name="articleId"/>.
+        /// </summary>
+        /// <param name="articleId">The Id of the Article.</param>
+        /// <returns>Returns the total amount of views for the given article.</returns>
+        int AddView(int articleId);
+
+        /// <summary>
+        ///     Increment the amount of views in the Article
+        ///     with Id equal to <paramref name="articleId"/> asynchronously.
+        /// </summary>
+        /// <param name="articleId">The Id of the Article.</param>
+        /// <returns>Returns the total amount of views for the given article.</returns>
+        Task<int> AddViewAsync(int articleId);
+    }
+}
