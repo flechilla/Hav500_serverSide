135e012879a8ae29a9a08e586f4b2d0258960114
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-08-21 20:01:10 -0400

    Implement the entities related to the Articles and other contents

---
+++ src/Havana500.Domain/Models/ArticleContentTag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havana500.Domain
+{
+    /// <summary>
+    ///     Pivot table for the relation between
+    ///     <see cref="Article"/> and <see cref="ContentTag"/>
+    /// </summary>
+    public class ArticleContentTag
+    {
+        /// <summary>
+        ///     Gets or sets the FK of the Article
+        /// </summary>
+        public int ArticleId { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the FK of ContentTag
+        /// </summary>
+        public int ContentTagId { get; set; }
+    }
+}
---
+++ src/Havana500.Domain/Models/ContentTag.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.Domain.Base;
+
+namespace Havana500.Domain
+{
+    /// <summary>
+    ///     Represent a tag that can be assigned to any
+    ///     content (i.e Section, Article...)
+    /// </summary>
+    public class ContentTag : AuditableAndTrackableEntity<int>
+    {
+        /// <summary>
+        ///     Gets or sets the name of the Tag.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value that indicates the amount
+        ///     of times that the tag has been used.
+        /// </summary>
+        public int AmountOfContent { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the articles that contains this tag.
+        /// </summary>
+        public ICollection<Article> Articles { get; set; }
+    }
+}
