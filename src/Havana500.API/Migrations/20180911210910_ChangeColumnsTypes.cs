5e68d7f6d0c8c2eb75b316e8018a1a95c084593c
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-11 17:46:34 -0400

    Adds migrations for changing columns type

---
+++ src/Havana500.API/Migrations/20180911210910_ChangeColumnsTypes.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class ChangeColumnsTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Views",
+                table: "Sections",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AmountOfComments",
+                table: "Sections",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AmountOfComments",
+                table: "Articles",
+                nullable: false,
+                oldClrType: typeof(long));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Views",
+                table: "Sections",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AmountOfComments",
+                table: "Sections",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AmountOfComments",
+                table: "Articles",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
