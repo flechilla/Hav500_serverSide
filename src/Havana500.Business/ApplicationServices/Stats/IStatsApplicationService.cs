1f1722101d386bf7879b25f6a5320592e4ea9a61
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-11 16:29:40 -0400

    Implements the AppService and Repository for the Stats

---
+++ src/Havana500.Business/ApplicationServices/Stats/IStatsApplicationService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.Domain;
+
+namespace Havana500.Business.ApplicationServices.Stats
+{
+    /// <summary>
+    ///     Declares the methods that are used to work with the Stats.
+    /// </summary>
+    public interface IStatsApplicationService
+    {
+        /// <summary>
+        ///     Returns the total amount of comments in the system.
+        /// </summary>
+        /// <returns></returns>
+        int GetTotalArticles();
+
+        /// <summary>
+        ///     Returns the total amount of articles in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        int GetTotalNewArticles(int lastDays=7);
+
+        /// <summary>
+        ///     Returns the total amount of comments in the system.
+        /// </summary>
+        /// <returns></returns>
+        int GetTotalComments();
+
+        /// <summary>
+        ///     Returns the total amount of new comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Comments</param>
+        /// <returns></returns>
+        int GetTotalNewComments(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the total amount of approved comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the comments</param>
+        /// <returns></returns>
+        int GetTotalApprovedComments(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the total amount of not approved comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the comments</param>
+        /// <returns></returns>
+        int GetTotalNotApprovedComments(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the total amount of media files in the system.
+        /// </summary>
+        /// <returns></returns>
+        int GetTotalMediaFiles();
+
+        /// <summary>
+        ///     Returns the total amount of new media files in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        int GetTotalNewMediaFiles(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the total amount of article's visits in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        int GetTotalArticleVisits(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the total amount of users in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate</param>
+        /// <returns></returns>
+        int GetTotalUsers(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the Article with more views in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate</param>
+        /// <returns></returns>
+        Article GetArticleWithMoreViews(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns with Article with more comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        Article GetArticleWithMoreComments(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the Section with more comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate</param>
+        /// <returns></returns>
+        Domain.Section GetSectionNameWithMoreComments(int lastDays = 7);
+
+        /// <summary>
+        ///     Returns the Section with more Views in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        Domain.Section GetSectionNameWithMoreViews(int lastDays = 7);
+    }
+}
---
+++ src/Havana500.Business/ApplicationServices/Stats/StatsApplicationService.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.DataAccess.Repositories.Stats;
+using Havana500.Domain;
+
+namespace Havana500.Business.ApplicationServices.Stats
+{
+    public class StatsApplicationService : IStatsApplicationService
+    {
+        public IStatsRepository Repository { get; }
+
+        public StatsApplicationService(IStatsRepository repository)
+        {
+            Repository = repository;
+        }
+
+        /// <summary>
+        ///     Returns the total amount of comments in the system.
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotalArticles()
+        {
+            return Repository.GetTotalArticles();
+        }
+
+        /// <summary>
+        ///     Returns the total amount of articles in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        public int GetTotalNewArticles(int lastDays = 7)
+        {
+            return Repository.GetTotalNewArticles();
+        }
+
+        /// <summary>
+        ///     Returns the total amount of comments in the system.
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotalComments()
+        {
+            return Repository.GetTotalComments();
+        }
+
+        /// <summary>
+        ///     Returns the total amount of new comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Comments</param>
+        /// <returns></returns>
+        public int GetTotalNewComments(int lastDays = 7)
+        {
+            return Repository.GetTotalNewComments(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the total amount of approved comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the comments</param>
+        /// <returns></returns>
+        public int GetTotalApprovedComments(int lastDays = 7)
+        {
+            return Repository.GetTotalApprovedComments(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the total amount of not approved comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the comments</param>
+        /// <returns></returns>
+        public int GetTotalNotApprovedComments(int lastDays = 7)
+        {
+            return Repository.GetTotalNotApprovedComments(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the total amount of media files in the system.
+        /// </summary>
+        /// <returns></returns>
+        public int GetTotalMediaFiles()
+        {
+            return Repository.GetTotalMediaFiles();
+        }
+
+        /// <summary>
+        ///     Returns the total amount of new media files in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        public int GetTotalNewMediaFiles(int lastDays = 7)
+        {
+            return Repository.GetTotalNewMediaFiles(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the total amount of article's visits in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        public int GetTotalArticleVisits(int lastDays = 7)
+        {
+            return Repository.GetTotalArticleVisits(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the total amount of users in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate</param>
+        /// <returns></returns>
+        public int GetTotalUsers(int lastDays = 7)
+        {
+            return Repository.GetTotalUsers(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the Article with more views in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate</param>
+        /// <returns></returns>
+        public Article GetArticleWithMoreViews(int lastDays = 7)
+        {
+            return Repository.GetArticleWithMoreViews(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns with Article with more comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        public Article GetArticleWithMoreComments(int lastDays = 7)
+        {
+            return Repository.GetArticleWithMoreComments(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the Section with more comments in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate</param>
+        /// <returns></returns>
+        public Domain.Section GetSectionNameWithMoreComments(int lastDays = 7)
+        {
+            return Repository.GetSectionNameWithMoreComments(lastDays);
+        }
+
+        /// <summary>
+        ///     Returns the Section with more Views in the system.
+        /// </summary>
+        /// <param name="lastDays">The amount of days to calculate the new Articles</param>
+        /// <returns></returns>
+        public Domain.Section GetSectionNameWithMoreViews(int lastDays = 7)
+        {
+            return Repository.GetSectionNameWithMoreViews(lastDays);
+        }
+    }
+}
