376036ac912c1139e56fcc3c5acdac6ba17087f1
NachoVazquez <nachovc1410@gmail.com>
2018-09-13 13:33:10 -0400

    Creates SectionController (API)

---
+++ src/Havana500.API/Controllers/Api/SectionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Havana500.Business.ApplicationServices.Section;
+using Havana500.Domain;
+using Havana500.Models.SectionViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Havana500.Controllers.Api
+{
+    public class SectionController : BaseApiController<ISectionsApplicationService, Section, int, SectionBaseViewModel, SectionCreateViewModel, SectionCreateViewModel, SectionIndexViewModel>
+    {
+        public SectionController(ISectionsApplicationService appService, IMapper mapper) : base(appService, mapper)
+        {
+        }
+    }
+}
