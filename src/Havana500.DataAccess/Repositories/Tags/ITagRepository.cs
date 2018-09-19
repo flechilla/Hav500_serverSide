e893b1f1aedf8aff7a82df384953e0ec973ba862
NachoVazquez <nachovc1410@gmail.com>
2018-09-19 13:09:48 -0400

    Implement ContentTagRepository

---
+++ src/Havana500.DataAccess/Repositories/Tags/ITagRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.Domain;
+
+namespace Havana500.DataAccess.Repositories.Tags
+{
+    public interface ITagRepository : IBaseRepository<ContentTag, int>
+    {
+    }
+}
