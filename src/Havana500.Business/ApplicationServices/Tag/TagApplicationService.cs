93770528039b786cc279c981fcc95a18f5678231
NachoVazquez <nachovc1410@gmail.com>
2018-09-19 13:10:08 -0400

    Implement ContentTagApplicationService

---
+++ src/Havana500.Business/ApplicationServices/Tag/TagApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.Business.Base;
+using Havana500.DataAccess.Repositories;
+using Havana500.DataAccess.Repositories.Tags;
+using Havana500.Domain;
+
+namespace Havana500.Business.ApplicationServices.Tag
+{
+    public class TagApplicationService : BaseApplicationService<ContentTag, int>, ITagApplicationService
+    {
+        public TagApplicationService(ITagRepository repository) : base(repository)
+        {
+        }
+
+        public new ITagRepository Repository => base.Repository as ITagRepository;
+    }
+}
