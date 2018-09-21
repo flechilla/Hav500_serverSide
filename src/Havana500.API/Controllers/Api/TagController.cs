a995c27e5d57b9437c932461176a2a4203e0525d
NachoVazquez <nachovc1410@gmail.com>
2018-09-21 15:33:15 -0400

    Use TagIndeViewModel in Controller

--- src/Havana500.API/Controllers/Api/TagController.cs
+++ src/Havana500.API/Controllers/Api/TagController.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Havana500.Business.ApplicationServices.Tag;
 using Havana500.Domain;
 using Havana500.Models.TagViewModels;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
 namespace Havana500.Controllers.Api
 {
-    public class TagController : BaseApiController<ITagApplicationService, ContentTag, int, TagBaseViewModel, TagBaseViewModel, TagBaseViewModel, TagBaseViewModel>
+    public class TagController : BaseApiController<ITagApplicationService, ContentTag, int, TagBaseViewModel, TagBaseViewModel, TagBaseViewModel, TagIndexViewModel>
     {
         public TagController(ITagApplicationService appService, IMapper mapper) : base(appService, mapper)
         {
