0053ed74ea589077348e5739f609df13ce9fef5d
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-24 15:59:21 -0400

    Implements class with the static method to update the Weight in the Articles table

---
+++ src/Havana500.DataAccess/Jobs/ArticleBackgroundJobs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Havana500.DataAccess.Contexts;
+using Havana500.Domain;
+
+namespace Havana500.DataAccess.Jobs
+{
+    /// <summary>
+    ///     Contains the methods that are used to run backgraound Jobs
+    ///     for the <see cref="Article"/> entity.
+    /// </summary>
+    public static class ArticleBackgroundJobs
+    {
+        public static async Task UpdateWeightColumn(Havana500DbContext context)
+        {
+            var unitOfWork = new UnitOfWork.SqlUnitOfWork(context);
+
+            var query = @"
+USE Havana500;
+GO
+
+EXEC usp_updateArticlesWeight;               
+            ";
+            int result = -1;
+            try
+            {
+                await unitOfWork.QueryFirstAsync<int>(query);
+            }
+            finally
+            {
+                unitOfWork.Dispose();
+            }
+        }
+    }
+}
