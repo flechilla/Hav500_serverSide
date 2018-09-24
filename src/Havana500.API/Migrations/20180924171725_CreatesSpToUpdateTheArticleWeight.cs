45ee0af43d9dc0b832cdef82d7edf2cb9f22f8f2
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-24 13:23:52 -0400

    Adds migration with SP to update the Weight column in the Articles table

---
+++ src/Havana500.API/Migrations/20180924171725_CreatesSpToUpdateTheArticleWeight.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class CreatesSpToUpdateTheArticleWeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Adriano Flechilla
+-- Email: a.flechilla91@gmail.com
+-- Create date: 24/9/2018
+-- Description:	This SP is used to update the column Weight of Articles. 
+-- =============================================
+USE [Havana500];
+GO
+
+IF OBJECT_ID('usp_updateArticlesWeight') IS NOT NULL BEGIN
+	DROP PROC usp_updateArticlesWeight;
+END
+GO
+
+CREATE PROC dbo.usp_updateArticlesWeight AS
+	DECLARE @max_weight REAL;
+
+	UPDATE [dbo].[Articles]
+	SET Weight = ((Views + 1.1) * (AmountOfComments + 1.1) * (EditorWeight)) / (DATEDIFF(DAY, StartDateUtc, GETDATE())*10)
+
+	SELECT TOP 1 @max_weight = A.Weight
+	FROM [dbo].[Articles] AS A
+	ORDER BY A.Weight DESC;
+
+	UPDATE [dbo].[Articles]
+	SET Weight =  (Weight / @max_weight) * 100;  
+
+	RETURN 0;
+GO
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROC usp_updateArticlesWeight;");
+        }
+    }
+}
