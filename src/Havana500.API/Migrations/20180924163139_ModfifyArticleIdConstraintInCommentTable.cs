a69dc75d5d22c6a25231f99512cc41ca959cfdbf
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-24 13:09:58 -0400

    Adds migration to modify the constraint of the ArticleId in the Comments table
    Because in the future the comments can be added to other entities beside
    the Articles, with this migration we accept the null value in the ArticleId column

---
+++ src/Havana500.API/Migrations/20180924163139_ModfifyArticleIdConstraintInCommentTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class ModfifyArticleIdConstraintInCommentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArticleId",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ArticleId",
+                table: "Comments",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
