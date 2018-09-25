0be8e5e244c88677e8b3b16f97ce06f585d312f7
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-25 17:45:54 -0400

    Adds migration with the changes for the column

---
+++ src/Havana500.API/Migrations/20180925213618_AddsLanguageCultureColumnToTables.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class AddsLanguageCultureColumnToTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageCulture",
+                table: "Sections",
+                nullable: true,
+                defaultValue: "en");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageCulture",
+                table: "ContentTags",
+                nullable: true,
+                defaultValue: "en");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LanguageCulture",
+                table: "Articles",
+                nullable: true,
+                defaultValue: "en");
+
+            migrationBuilder.Sql(@"USE Havana500
+UPDATE Articles
+SET LanguageCulture = 'en'
+
+UPDATE Sections
+SET LanguageCulture = 'en'
+
+UPDATE ContentTags
+SET LanguageCulture = 'en'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LanguageCulture",
+                table: "Sections");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageCulture",
+                table: "ContentTags");
+
+            migrationBuilder.DropColumn(
+                name: "LanguageCulture",
+                table: "Articles");
+        }
+    }
+}
