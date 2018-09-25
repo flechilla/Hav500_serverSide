392e5482fc7d7be68b8ca92b2add96fa5a2e2f3c
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-09-25 17:45:08 -0400

    Implements interface with the definition of the LanguageCulture property

---
+++ src/Havana500.Domain/Models/ILanguage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havana500.Domain.Models
+{
+    /// <summary>
+    ///     Declares the property to identity
+    ///     the language of the content.
+    /// </summary>
+    public interface ILanguage
+    {
+        /// <summary>
+        ///     Gets or sets the Culture for the current
+        ///     entity
+        /// </summary>
+        /// <remarks>
+        ///     The value of this property is in neutral
+        ///     culture. Ex: 'es', 'en' or 'fr'
+        /// </remarks>
+        string LanguageCulture { get; set; }
+    }
+}
