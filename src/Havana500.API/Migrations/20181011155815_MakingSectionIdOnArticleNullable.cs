d0817b9ab653173240217428175ff37aac33a74d
NachoVazquez <nachovc1410@gmail.com>
2018-10-11 13:45:18 -0400

    Update Article to avoid SectionId to be required

---
+++ src/Havana500.API/Migrations/20181011155815_MakingSectionIdOnArticleNullable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class MakingSectionIdOnArticleNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Sections_SectionId",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SectionId",
+                table: "Articles",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Sections_SectionId",
+                table: "Articles",
+                column: "SectionId",
+                principalTable: "Sections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_Sections_SectionId",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SectionId",
+                table: "Articles",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_Sections_SectionId",
+                table: "Articles",
+                column: "SectionId",
+                principalTable: "Sections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
