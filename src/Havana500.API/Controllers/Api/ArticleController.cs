7e804b11fdd2cddfd496cb4722740b3bcb446836
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-14 23:31:18 -0400

    Remove unused code

--- src/Havana500.API/Controllers/Api/ArticleController.cs
+++ src/Havana500.API/Controllers/Api/ArticleController.cs
@@ -151,16 +151,6 @@ namespace Havana500.Controllers.Api
 
             var articlesVM = Mapper.Map<IEnumerable<ArticleBasicDataViewModel>>(articles);
 
-            //just for testing purposes
-        //    foreach (var article in articlesVM)
-        //    {
-        //        article.MainPicture = new API.Models.PictureViewModels.BasePictureViewModel(){
-        //            RelativePath = "/articlesUploadImages/590/mainPicture.jpg",
-        //            SeoFileName = "20180626_105031",
-        //            MimeType = "image/jpeg"
-        //        };
-        //    }
-
             return Ok(articlesVM);
         }
 
