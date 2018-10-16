02980cfd824c11db5e68e635aa2d428d44e0e132
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-16 19:56:17 -0400

    Implements the seed for the marketin images
    Now we create 10 items per each level

---
+++ src/Havana500.DataAccess/Seeds/MarketingPictureSeeds.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.DataAccess.Contexts;
+using Havana500.Domain.Models.Media;
+using SeedEngine.Core;
+
+namespace Havana500.DataAccess.Seeds
+{
+    public class MarketingPictureSeeds : ISeed<Havana500DbContext>
+    {
+        public void AddOrUpdate(Havana500DbContext context, int amountOfObjects = 20)
+        {
+            var marketingImages = new List<Picture>(20);
+
+            for (int i = 0; i < 10; i++)
+            {
+                marketingImages.Add(new Picture()
+                {
+                    RelativePath = "localhost:5000/marketingImages/testMarketingImage.jpg",
+                    PictureType = PictureType.FirstLevelMarketing,
+                    IsNew = true,
+                    IsActive = true,
+                    MimeType = "image/jpeg",
+                    PictureExtension = "jpeg",
+                    SeoFilename = "SeedImage"
+                });
+                marketingImages.Add(new Picture()
+                {
+                    RelativePath = "localhost:5000/marketingImages/testMarketingImage.jpg",
+                    PictureType = PictureType.SecondaryLevelMarketing,
+                    IsNew = true,
+                    IsActive = true,
+                    MimeType = "image/jpeg",
+                    PictureExtension = "jpeg",
+                    SeoFilename = "SeedImage"
+                });
+                marketingImages.Add(new Picture()
+                {
+                    RelativePath = "localhost:5000/marketingImages/testMarketingImage.jpg",
+                    PictureType = PictureType.TertiaryLevelMarketing,
+                    IsNew = true,
+                    IsActive = true,
+                    MimeType = "image/jpeg",
+                    PictureExtension = "jpeg",
+                    SeoFilename = "SeedImage"
+                });
+            }
+            context.PIctures.AddRange(marketingImages);
+            context.SaveChanges();
+        }
+
+        public int OrderToByApplied => 5;
+    }
+}
