e6ae524cd0ff5b7fac75336ce4905c02e316a4e0
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-16 18:56:34 -0400

    Adds migrations to fix the relation between Article-(Pictures, Comments)
    Now it's configured to delete the related entities in cascade

---
+++ src/Havana500.API/Migrations/20181016224200_configureArticlePictureRelation.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class configureArticlePictureRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PIctures_Articles_ArticleId",
+                table: "PIctures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PIctures_ArticleId",
+                table: "PIctures");
+
+            migrationBuilder.DropColumn(
+                name: "ArticleId",
+                table: "PIctures");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ArticleId",
+                table: "PIctures",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PIctures_ArticleId",
+                table: "PIctures",
+                column: "ArticleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PIctures_Articles_ArticleId",
+                table: "PIctures",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Havana500.API/Migrations/20181016225102_configureArticleCommentsRelation.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Havana500.Migrations
+{
+    public partial class configureArticleCommentsRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Articles_ArticleId",
+                table: "Comments",
+                column: "ArticleId",
+                principalTable: "Articles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
