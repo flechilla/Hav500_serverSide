4cbf5c4f119d2d1534ab08d20f6da84456ec6362
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-10-16 20:16:00 -0400

    Implements Controller with action to get the marketing images
    NOTE:
    - Have to send the level and the amount of picture.
    - The level is a numeric value with the possible options: 1, 2, 3

---
+++ src/Havana500.API/Controllers/Api/MarketingPicturesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Havana500.API.Models.PictureViewModels;
+using Havana500.Business.ApplicationServices.Pictures;
+using Havana500.Domain.Models.Media;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Havana500.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/MarketingPictures")]
+    public class MarketingPicturesController : BaseApiController<IPicturesApplicationService, Picture, int,
+        BasePictureViewModel, BasePictureViewModel, BasePictureViewModel, BasePictureViewModel>
+    {
+        public MarketingPicturesController(IPicturesApplicationService appService, IMapper mapper) : base(appService,
+            mapper)
+        {
+        }
+
+        [HttpGet("GetImagesByLevel")]
+        public async Task<IActionResult> GetImagesByLevel(int level, int count)
+        {
+            PictureType pictureType = PictureType.FirstLevelMarketing;
+            switch (count)
+            {
+                case 1:
+                    pictureType = PictureType.FirstLevelMarketing;
+                    break;
+                case 2:
+                    pictureType = PictureType.SecondaryLevelMarketing;
+                    break;
+                case 3:
+                    pictureType = PictureType.TertiaryLevelMarketing;
+                    break;
+            }
+            var result = await ApplicationService.GetByTypeASync(pictureType, count);
+
+            var resultVM = Mapper.Map<IEnumerable<BasePictureViewModel>>(result);
+
+            return Ok(resultVM);
+        }
+    }
+}
