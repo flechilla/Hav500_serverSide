1a55ac86dae0f64df056e067802e8d602afc43b8
NachoVazquez <nachovc1410@gmail.com>
2018-10-17 11:06:53 -0400

    Add MarktingContentAppService

---
+++ src/Havana500.Business/ApplicationServices/MarketingContent/IMarketingContentApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.Business.Base;
+
+namespace Havana500.Business.ApplicationServices.MarketingContent
+{
+    public interface IMarketingContentApplicationService : IBaseApplicationService<Domain.MarketingContent, int>
+    {
+    }
+}
---
+++ src/Havana500.Business/ApplicationServices/MarketingContent/MarketingContentApplicationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Havana500.Business.Base;
+using Havana500.DataAccess.Repositories;
+
+namespace Havana500.Business.ApplicationServices.MarketingContent
+{
+    public class MarketingContentApplicationService : BaseApplicationService<Domain.MarketingContent, int>, IMarketingContentApplicationService
+    {
+        public MarketingContentApplicationService(IBaseRepository<Domain.MarketingContent, int> repository) : base(repository)
+        {
+        }
+    }
+}
