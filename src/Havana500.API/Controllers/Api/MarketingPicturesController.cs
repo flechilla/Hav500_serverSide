951835ffa07d4b4dfc816c5cfba616993d537d5e
NachoVazquez <nachovc1410@gmail.com>
2018-10-18 12:47:44 -0400

    Create endpoints for TagManipulation and Marketing image upload

--- src/Havana500.API/Controllers/Api/MarketingPicturesController.cs
+++ src/Havana500.API/Controllers/Api/MarketingPicturesController.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using AutoMapper;
 using Havana500.API.Models.PictureViewModels;
 using Havana500.Business.ApplicationServices.Pictures;
+using Havana500.Business.ApplicationServices.Tag;
+using Havana500.Domain;
 using Havana500.Domain.Models.Media;
+using Havana500.Models.PictureContentTagViewModels;
+using Havana500.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace Havana500.Controllers.Api
 {
@@ -15,12 +19,16 @@ namespace Havana500.Controllers.Api
     public class MarketingPicturesController : BaseApiController<IPicturesApplicationService, Picture, int,
         BasePictureViewModel, BasePictureViewModel, BasePictureViewModel, BasePictureViewModel>
     {
-        public MarketingPicturesController(IPicturesApplicationService appService, IMapper mapper) : base(appService,
+        private readonly ITagApplicationService _tagApplicationService;
+        private readonly ImageService _imageService;
+        public MarketingPicturesController(IPicturesApplicationService appService, IMapper mapper, ITagApplicationService tagApplicationService, ImageService imageService) : base(appService,
             mapper)
         {
+            _tagApplicationService = tagApplicationService;
+            _imageService = imageService;
         }
 
-        [HttpGet()]
+        [HttpGet]
         public async Task<IActionResult> GetImagesByLevel(int level, int count)
         {
             PictureType pictureType = PictureType.FirstLevelMarketing;
@@ -43,12 +51,96 @@ namespace Havana500.Controllers.Api
             return Ok(resultVM);
         }
 
+        #region Admin
         [Area("Admin")]
         [HttpPost]
-        public async Task<IActionResult> CreateTemporaryArticle()
+        [ProducesResponseType(201)]
+        public async Task<IActionResult> CreateTemporaryPicture()
         {
             var empty = new BasePictureViewModel();
             return await base.Post(empty);
         }
+
+        [Area("Admin")]
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> AddTagToPicture([FromBody, Required]PictureContentTagViewModel pictureContentTag)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!ApplicationService.Exists(pictureContentTag.PictureId))
+            {
+                return NotFound("Picture with id: " + pictureContentTag.PictureId);
+            }
+
+            if (!_tagApplicationService.Exists(pictureContentTag.ContentTagId))
+            {
+                return NotFound("Tag with id: " + pictureContentTag.ContentTagId);
+            }
+
+            var result =
+                await ApplicationService.AddPictureContentTagAsync(Mapper.Map<PictureContentTag>(pictureContentTag));
+
+            await ApplicationService.SaveChangesAsync();
+
+            var viewModelRes = Mapper.Map<PictureContentTagViewModel>(result);
+
+
+            return CreatedAtAction("POST", viewModelRes);
+
+        }
+
+        [Area("Admin")]
+        [HttpDelete]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> RemoveTagToPicture(int pictureId, int tagId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!ApplicationService.Exists(pictureId))
+            {
+                return NotFound("Picture with id: " + pictureId);
+            }
+
+            if (!_tagApplicationService.Exists(tagId))
+            {
+                return NotFound("Tag with id: " + tagId);
+            }
+
+            await ApplicationService.RemovePictureContentTagAsync(new PictureContentTag { PictureId = pictureId, ContentTagId = tagId });
+
+            await ApplicationService.SaveChangesAsync();
+
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadMarketingPicture(int marketingId, IFormFile file)
+        {
+            IUrlHelper urlHelper = new UrlHelper(ControllerContext);
+            var domain = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}";
+
+            var result = await _imageService.UploadMarketingImageFile(file, marketingId, urlHelper, domain);
+
+
+            if (result != null)
+                return Ok(result);
+            //return Created("The image was upload.");//TODO: get the relative path and send it back
+
+            return new StatusCodeResult(500);
+        }
+
+        #endregion
     }
 }
