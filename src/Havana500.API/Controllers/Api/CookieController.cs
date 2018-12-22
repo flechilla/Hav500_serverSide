4405c3ba3ed6930bbe6404cf384536801a11df19
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-12-22 11:33:48 -0500

    Implement the controller to set the Language using the cookies

---
+++ src/Havana500.API/Controllers/Api/CookieController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Havana500.API.Controllers.Api
+{
+
+    public class CookieController : Controller
+    {
+    private const string _cookiesAcceptedKey = "CookiesAccepted";
+
+        [HttpPost]
+        public IActionResult AcceptCookies() 
+        {
+            Response.Cookies.Append(_cookiesAcceptedKey, true.ToString());
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string lang)
+        {
+            
+          Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(lang)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+
+                return Ok();
+        }
+    }
+}
