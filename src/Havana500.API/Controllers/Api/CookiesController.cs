683bcd1e1830c8ab15d691f03c8feaab801a16bc
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-12-23 10:54:36 -0500

    Minor changes

--- src/Havana500.API/Controllers/Api/CookiesController.cs
+++ src/Havana500.API/Controllers/Api/CookiesController.cs
@@ -10,26 +10,31 @@ namespace Havana500.API.Controllers.Api
     [Route("api/v1/[controller]/[action]")]
     public class CookiesController : Controller
     {
-    private const string _cookiesAcceptedKey = "CookiesAccepted";
+        private const string _cookiesAcceptedKey = "CookiesAccepted";
 
-        [HttpPost]
-        public IActionResult AcceptCookies() 
+        [HttpGet]
+        public IActionResult AcceptCookies()
         {
-            Response.Cookies.Append(_cookiesAcceptedKey, true.ToString());
+            Response.Cookies.Append(_cookiesAcceptedKey, true.ToString(),
+                new CookieOptions
+                {
+                    Expires = DateTimeOffset.UtcNow.AddYears(1)
+                });
             return Ok();
         }
 
         [HttpPost]
         public IActionResult SetLanguage([FromBody]LangViewModel lang)
         {
-            Console.WriteLine($"Lang param: {lang}");
-          Response.Cookies.Append(
-                CookieRequestCultureProvider.DefaultCookieName,
-                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(lang.Lang)),
-                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
-                );
-
-                return Ok();
+            Response.Cookies.Append(
+                  CookieRequestCultureProvider.DefaultCookieName,
+                  CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(lang.Lang)),
+                  new CookieOptions
+                  {
+                      Expires = DateTimeOffset.UtcNow.AddYears(1)
+                  }
+                  );
+            return Ok();
         }
     }
 }
