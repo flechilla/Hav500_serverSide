98aabccf4885b4c51fc04abd76ff5477685e623d
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2018-12-23 10:55:32 -0500

    Fix the action to get the tags
    Now there is an action that overrides to default one in order to get
    that ones with the user's lang

--- src/Havana500.API/Controllers/Api/TagController.cs
+++ src/Havana500.API/Controllers/Api/TagController.cs
@@ -1,7 +1,11 @@
-﻿using AutoMapper;
+﻿using System.Threading;
+using AutoMapper;
 using Havana500.Business.ApplicationServices.Tag;
 using Havana500.Domain;
 using Havana500.Models.TagViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Havana500.Controllers.Api
 {
@@ -9,6 +13,20 @@ namespace Havana500.Controllers.Api
     {
         public TagController(ITagApplicationService appService, IMapper mapper) : base(appService, mapper)
         {
+        }
+
+        public override IActionResult GetAll()
+        {
+            var lang = Thread.CurrentThread.CurrentCulture.Name;
+            var result = this.ApplicationService.
+                ReadAll(tag => tag.LanguageCulture == lang) 
+                            .Select(t => new TagBaseViewModel()
+                            {
+                                Name = t.Name,
+                                Id = t.Id
+                            }).ToArray();
+
+            return Ok(result);
         }
     }
 }
