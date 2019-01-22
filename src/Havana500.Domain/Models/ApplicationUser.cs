87037828798a2d8eef407fa9a4d2eab235eabaf7
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-22 10:23:01 -0500

    Add the role column to the User table

--- src/Havana500.Domain/Models/ApplicationUser.cs
+++ src/Havana500.Domain/Models/ApplicationUser.cs
@@ -14,5 +14,14 @@ namespace Havana500.Domain
         public string UserImageHRef { get; set; }
 
         public string UserImageLocalPath { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the value that represents the user's role
+        /// </summary>
+        /// <remarks>
+        ///     Because this is a pretty simple app, we'll have only one
+        ///     role per user.
+        /// </remarks>
+        public string Role { get; set; }
     }
 }
