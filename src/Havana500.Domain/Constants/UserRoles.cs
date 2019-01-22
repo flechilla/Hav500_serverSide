fa58e8782b6e29c217fd2d43975bb5552708a77f
Adriano Flechilla Frometa <a.flechilla91@gmail.com>
2019-01-22 10:23:25 -0500

    Implement static class that contains the different roles in the system

---
+++ src/Havana500.Domain/Constants/UserRoles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Havana500.Domain.Constants
+{
+    /// <summary>
+    ///     Contains the definition for the multiple
+    ///     roles in the system.
+    /// </summary>
+    public static class UserRoles
+    {
+        /// <summary>
+        ///     Indicates that the user has the max
+        ///     permissions
+        /// </summary>
+        public const string ADMIN = "Admin";
+
+        /// <summary>
+        ///     Indicates that the user has permission to 
+        ///     Create and Modify content
+        /// </summary>
+        public const string EDITOR = "Editor";
+
+        /// <summary>
+        ///     Indicate that the user has permission to 
+        ///     moderate the comments.
+        /// </summary>
+        public const string COMMMENT_MODERATOR = "CommentModerator";
+
+    }
+}
